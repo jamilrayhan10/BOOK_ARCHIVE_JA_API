ccf57c90d7819c7d8e09d9fdbe4289ed7e6ae889
jamilrayhan <rjamil336@gmail.com>
2021-09-02 19:43:05 +0600

    add-nav-bar

--- js/bookarchive.js
+++ js/bookarchive.js
@@ -34,10 +34,10 @@ const displayBook = (books) => {
   bookDetails?.forEach((book) => {
     const div = document.createElement("div");
     div.innerHTML = `
-                        <div class="card m-3" style="width: 18rem;">
-                            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="img">
+                        <div class="card m-3  " style="width: 18rem;">
+                            <img   class="h-50" src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : "img not found"}-M.jpg" class="card-img-top" alt="img">
                             <div class="card-body">
-                                <p> <b>Book Name:</b> ${book.title}</p>
+                                <h4> Name: ${book.title}</h4>
                                 <p> <b>Autthor:</b> ${book.author_name ? book.author_name : "not found author name"}</p>
                                 <p> <b>Publisher:</b> ${book.publisher ? book.publisher : "NOT FOUND fublisher"}</p>
                                 <p> <b>first publish year:</b> ${book.first_publish_year ? book.first_publish_year : "not found "}</p>
